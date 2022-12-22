475c55be874bb98331c6855b3721d2cd53d7c55b
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-12-22 15:46:29 +0530

    email sending remaining

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,17 +3,29 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  Button,
+  IconButton,
 } from "@chakra-ui/react";
-
-import arrowsvg from "../../images/icon-arrow.svg";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import "./form.css";
 
 export default function Navbar() {
   return (
     <Box>
       <InputGroup>
-        <Input placeholder="mysite" />
-        <InputRightElement children={<Button>></Button>} />
+        <Input placeholder="Email Address" className="inputgrp" type="email" />
+        <InputRightElement
+          children={
+            <IconButton
+              aria-label="Send Email"
+              icon={<ChevronRightIcon />}
+              className="iconbtn"
+              onClick={() => alert("click!")}
+              type="submit"
+            />
+          }
+          className="iconre"
+          minW={{ base: "70px", md: "90px" }}
+        />
       </InputGroup>
     </Box>
   );
